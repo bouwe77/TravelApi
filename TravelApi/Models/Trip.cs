091d8e4e0175ad357e73313ae4a344fd2de9ae76
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2017-01-12 10:40:04 +0100

    .

--- TravelApi/Models/Trip.cs
+++ TravelApi/Models/Trip.cs
@@ -10,18 +10,9 @@ namespace TravelApi.Models
       [Ignore]
       public Person Person { get; set; }
 
-      public string FromLocationId { get; set; }
-
-      [Ignore]
-      public Location FromLocation { get; set; }
-
-      public string ToLocationId { get; set; }
-
-      [Ignore]
-      public Location ToLocation { get; set; }
-
-      public string CreatedAt { get; set; }
+      public string DestinationId { get; set; }
       
-      public string Transportation { get; set; }
+      [Ignore]
+      public Destination Destination { get; set }
    }
 }
