64262fe72e680c313eeb04775eb848a4a4667d85
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2017-01-25 12:54:03 +0100

    .

--- TravelApi/Models/Entity.cs
+++ TravelApi/Models/Entity.cs
@@ -16,7 +16,9 @@ namespace TravelApi.Models
 
       protected bool Equals(Entity other)
       {
-         return string.Equals(Id, other.Id);
+         //TODO Ik heb hier een case insensitive compare van gemaakt. Even checken of dit inderdaad moet.
+         //TODO Ook doorvoeren in FMG2?
+         return string.Equals(Id, other.Id, StringComparison.OrdinalIgnoreCase);
       }
 
       public override bool Equals(object obj)
